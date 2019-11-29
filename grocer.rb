8525a98457d798df50c2b5c276f12a0330e86759
melia miller <meliajm@gmail.com>
2019-11-29 21:17:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -76,9 +76,9 @@ def apply_coupons(cart, coupons)
     j = 0 
     while j < cart_new.length do 
       binding.pry
-      if item_i == coupons[j][:item] && coupons[j][:count] > 0 
-        cart_new[i][:count] -= 1 
-        coupons[j][:count] -= 1 
+      if item_i == coupons[j][:item] && coupons[j][:num] > 0 
+        cart_new[i][:num] -= 1 
+        coupons[j][:num] -= 1 
         cart_new[i][:item] = "#{item} W/COUPON"
         cart_new[i][:count] += 1
         cart_new[i][:price] = coupons[j][:price]
